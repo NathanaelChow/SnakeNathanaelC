d97faf767fe8f43b873fb09c771d67efa75b4b8c
NathanaelChow <nhchowder@gmail.com>
2014-10-07 09:20:03 -0700

    Working on video 10

--- test/js/snake.js
+++ test/js/snake.js
@@ -5,11 +5,19 @@ var screenWidth;
 var screenHeight;
 
 function gameInitialize () {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
     
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 function gameLoop () {
     
 }
+
 function gameDraw () {
-    
+    context.fillStyle
 }
